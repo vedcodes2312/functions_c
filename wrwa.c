3699adf9f8e79e910f7ef10765b5c2b4391d10e4
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-08 07:37:38 +0530

    Update wrwa.c

--- wrwa.c
+++ wrwa.c
@@ -5,14 +5,14 @@
 int sum(int x,int y);//declaration
 
 void main()
-{
+{                  //calling     
     int a=5,b=6;
     int result;
     result = sum(a,b);
     printf("\n sum%d",result);
 }
 
-int sum(int x,int y)
+int sum(int x,int y)//definition 
 {
     int m=x,n=y;
     return (m+n);
