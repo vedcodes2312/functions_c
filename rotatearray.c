3b65a78f15b33691d7e9f95fc181db36e4f10575
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-21 10:34:38 +0530

    Add files via upload

---
+++ rotatearray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void rotateArray(int arr[], int n, int k) {
+    int temp[n];
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+int main() {
+    int n, k;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number of steps to rotate: ");
+    scanf("%d", &k);
+
+    rotateArray(arr, n, k);
+
+    printf("Rotated array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
